e1d823eefdc8ce8a16563efee48a06e8f4761028
Mariana Rosillo <rosillo@ucsb.edu>
2024-03-10 17:24:47 -0700

    website changes

--- about-me/src/App.js
+++ about-me/src/App.js
@@ -4,43 +4,51 @@ import React from 'react';
 import SSCCClogo from './SSCCClogo.png';
 import IvanInDC from './IvanInDC.JPG';
 import './App.css';
-import { RoutesComponent } from './RoutesComponent';// Update the import statement in App.js
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+// import { RoutesComponent } from './RoutesComponent';// Update the import statement in App.js
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import BrowserRouter
+import HomePage from './components/HomePage';
+import AboutMe from './components/AboutMePage';
+import Endorsements from './components/EndorsementsPage';
+import Platform from './components/PlatformPage';
+import Involvement from './components/InvolvementPage';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-
-      {/* Your menu section goes here */}
-      <nav className="nav ul">
+      {/* <nav className="nav ul">
           <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About Me</a></li>
-            <li><a href="#services">Endorsements</a></li>
-            <li><a href="#contact">Platform</a></li>
-            <li><a href="#contact">Involvement</a></li>
+            <li><a href="#endorsements">Endorsements</a></li>
+            <li><a href="#platform">Platform</a></li>
+            <li><a href="#involvement">Involvement</a></li>
           </ul>
-        </nav>
-        {/* End of menu section */}
-      {/* <Router>
-        <RoutesComponent />
-      </Router> */}
+        </nav> */}
 
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+      <Router>
+        <div className='content'>
+          <Navbar />
+        </div>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/about-me" component={AboutMe} />
+            <Route path="/endorsements" component={Endorsements} />
+            <Route path="/platform" component={Platform} />
+            <Route path="/involvement" component={Involvement} />
+          </Switch>
+        
+      </Router>
+      {/* <header className="App-header">
         {<img src={SSCCClogo} className="logo" alt="Logo" />}
-        {/* <p>
-          This is where change begins!
-        </p>
-        <p> 
-          My name is Ivan Hernandez and I am your SSCCC Presidential Candidate. </p>
-          <p>I am here to support you!</p> */}
 
-      </header>
 
-      <body className="app-body"> 
-      {<img src={IvanInDC} className="home-image" alt="Home-image" />}
-        </body>
+      </header> */}
+
+      {/* <body className="app-body"> 
+        {<img src={IvanInDC} className="home-image" alt="Home-image" />}
+      </body> */}
     </div>
   );
 }
--- about-me/src/components/AboutMePage.js
+++ about-me/src/components/AboutMePage.js
@@ -1,29 +1,5 @@
-// import React from 'react';
-// import Routes from './Routes';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Routes />
-//     </div>
-//   );
-// }
-
-// export default App;
-// AboutMe.js
 import React from 'react';
-
-// function AboutMe() {
-//   return (
-//     <div>
-//       <h1>About Me</h1>
-//       <p>This is the About Me page content.</p>
-//     </div>
-//   );
-// }
-
-// export default AboutMe;
-
 
 const AboutMePage = () => {
   return (
